b75f6676ca63e479e8534be1798508402a4d1b1f
Madhih <madhih05@gmail.com>
2024-06-09 19:48:26 +0530

    did some stuff to the multipliers

--- GuessingGame/GuessingGame.py
+++ GuessingGame/GuessingGame.py
@@ -1,4 +1,5 @@
 from random import randint as rand
+import sys
 
 def game_manager():
     remaining_turns=0
@@ -8,7 +9,7 @@ def game_manager():
         x=int(input("How hard do you want the game to be in the scale of 1-10?\n:"))
         if x>10:
             print("\nthe game can only handle up to a difficulty of 10")
-        sys_num=rand(x,5+int(x*1.2))
+        sys_num=rand(x,5+int(x*10))
 
         remaining_turns=3+int(x/2)
         if remaining_turns<1:
