bb50c762f659c66ced3117c9ac1157ddcb9fd72e
Madhih <madhih05@gmail.com>
2024-06-09 21:06:47 +0530

    final program to run on command screen

--- GuessingGame/GuessingGame.py
+++ GuessingGame/GuessingGame.py
@@ -9,7 +9,7 @@ def game_manager():
         x=int(input("How hard do you want the game to be in the scale of 1-10?\n:"))
         if x>10:
             print("\nthe game can only handle up to a difficulty of 10")
-        sys_num=rand(x,5+int(x*10))
+        sys_num=rand(x,5+int(x**1.9777))
 
         remaining_turns=3+int(x/2)
         if remaining_turns<1:
@@ -31,7 +31,7 @@ def game(y,num,rem):
         return(False,rem)
 
 x,y,z=game_manager()
-print("guess the number generated between the limit 1 to",5+int(x*10))
+print("guess the number generated between the limit 1 to",5+int(x**1.9777))
 state,remaining_turns=game(x,y,z)
 
 if state:
